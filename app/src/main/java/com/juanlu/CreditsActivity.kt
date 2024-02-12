c76fcbdf651ae2a7e1fc4485277424fc0525ba5d
Patrontrox <sgn_juanlu@protonmail.com>
2024-02-12 22:52:07 +0100

    Supressed a warning in order to improve building time

--- app/src/main/java/com/juanlu/CreditsActivity.kt
+++ app/src/main/java/com/juanlu/CreditsActivity.kt
@@ -9,7 +9,7 @@ import android.widget.Button
 import android.widget.TextView
 
 class CreditsActivity : AppCompatActivity() {
-    @SuppressLint("IntentReset", "MissingInflatedId")
+    @SuppressLint("IntentReset", "MissingInflatedId", "StringFormatInvalid")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_credits)
